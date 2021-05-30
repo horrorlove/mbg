193f40f8e89f86978e6c4714c7ce4e7e9f3be4bb
chamchamuvu <85019120+chamchamuvu@users.noreply.github.com>
2021-05-30 12:43:28 +0200

    Made theme and index draft

---
+++ JavaScript/layout.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  /* Toggle between adding and removing the "responsive" class to the navbar when the user clicks on the icon */
+function myFunction2() {
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
